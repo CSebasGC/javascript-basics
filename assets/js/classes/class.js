b26cd8b7ce5598b8a2ad31debc0f0c511fb63e07
Sebastian_GC <csebastiangomez@ucompensar.edu.co>
2025-02-19 09:23:28 -0500

    ver 1

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -11,9 +11,15 @@ class Persona{
         this.frase = frase;
         this.comida = comida;
     }
+    quienSoy(){
+        console.log(`Soy ${this.comida} y mi identidad es ${this.nombre}`);//` este simbolo se llama backtick y se saca con alt + 96
+    }
 }
 
 const spiderman = new Persona('Peter Parker', 'Spiderman','Soy tu amigable vecino Spiderman', 'Pizza');
 const ironman = new Persona('Tony Stark', 'Ironman','Yo soy Ironman', 'Pollo');
 console.log(spiderman);
 console.log(ironman);
+
+spiderman.quienSoy();
+ironman.quienSoy();
