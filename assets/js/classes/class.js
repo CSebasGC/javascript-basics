d38db92b19d3342aeba3ae7afc2d86fb13c483b2
Sebastian_GC <csebastiangomez@ucompensar.edu.co>
2025-02-19 12:26:48 -0500

    avance de clases y statics y private

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -1,25 +1,72 @@
 class Persona{
-    
+
+   static _conteo = 0;
+   static get conteo(){
+         return Persona._conteo + ' instancias';
+   }
+
+   static mensaje(){
+        console.log(this.nombre);//no se puede acceder a las propiedades de la clase
+        console.log('Hola a todos, soy un metodo estatico');
+   }
+
     nombre = ''; 
     codigo = '';
     frase = '';
     comida = '';
+
+
 
     constructor(nombre='Sin nombre', codigo='Sin codigo', frase='Sin frase', comida='Sin comida'){
         this.nombre = nombre
         this.codigo = codigo;
         this.frase = frase;
         this.comida = comida;
+
+        Persona._conteo++;//con esto se lleva el conteo de cuantas veces se ha instanciado la clase
     }
+
+    //set es para modificar y solo reciben un parametro
+    //get es para obtener
+    set setComidaFavorita(comida){
+        this.comida = comida.toUpperCase();
+    }
+    get getComidaFavorita(){
+        return `La comida favorita de ${this.nombre} es ${this.comida}`;
+    }
+
     quienSoy(){
         console.log(`Soy ${this.comida} y mi identidad es ${this.nombre}`);//` este simbolo se llama backtick y se saca con alt + 96
+    }
+
+    miFrase(){
+        console.log(`${this.codigo} dice: ${this.frase}`);
     }
 }
 
 const spiderman = new Persona('Peter Parker', 'Spiderman','Soy tu amigable vecino Spiderman', 'Pizza');
 const ironman = new Persona('Tony Stark', 'Ironman','Yo soy Ironman', 'Pollo');
+const antman = new Persona('Scott Lang', 'Antman','Antman es el super', 'Hamburguesa');
+
+//Persona._conteo = 2;
+
 console.log(spiderman);
 console.log(ironman);
 
 spiderman.quienSoy();
 ironman.quienSoy();
+
+spiderman.miFrase();
+ironman.miFrase();
+
+spiderman.setComidaFavorita = 'Tacos y no le gusta la comida de la tia May';
+//spiderman.nemesis = "Duende Verde";
+//console.log(spiderman);
+//console.log(spiderman.getComidaFavorita);
+console.log('Conteo estatico', Persona._conteo);
+console.log(Persona.conteo);
+Persona.mensaje();
+
+Persona.propiedadExterna = 'Hola Mundo';
+console.log(Persona.propiedadExterna);
+console.log(Persona);
