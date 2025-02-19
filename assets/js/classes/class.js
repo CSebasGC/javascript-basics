98b04a363cfa5df1c85131dd42c0112a36961094
Sebastian_GC <csebastiangomez@ucompensar.edu.co>
2025-02-18 21:01:20 -0500

    Ver 2.0

---
+++ assets/js/classes/class.js
@@ -0,0 +1,19 @@
+class Persona{
+    
+    nombre = ''; 
+    codigo = '';
+    frase = '';
+    comida = '';
+
+    constructor(nombre='Sin nombre', codigo='Sin codigo', frase='Sin frase', comida='Sin comida'){
+        this.nombre = nombre
+        this.codigo = codigo;
+        this.frase = frase;
+        this.comida = comida;
+    }
+}
+
+const spiderman = new Persona('Peter Parker', 'Spiderman','Soy tu amigable vecino Spiderman', 'Pizza');
+const ironman = new Persona('Tony Stark', 'Ironman','Yo soy Ironman', 'Pollo');
+console.log(spiderman);
+console.log(ironman);
